4f720d1ffe5cbafc74acf19407ea99a511bef698
brunogonzalez96 <gonzalezbrunoezequiel@gmail.com>
2024-06-18 23:35:21 -0300

    PreEntrega1Gonzalez

--- PreEntrega1Gonzalez/main.js
+++ PreEntrega1Gonzalez/main.js
@@ -1,13 +1,14 @@
 
     let Bienvenida = "Bienvenido a BEBIDAS GONZALEZ";
     alert (Bienvenida)
-    let IngresoDatosClientes = parseFloat(prompt("Por favor ingrese su nombre y apellido"));
-    let IngresoEdad = prompt("Por favor ingrese su edad");
+    let IngresoDatosClientes = (prompt("Por favor ingrese su nombre y apellido"));
+    let IngresoEdad = parseInt(prompt("Por favor ingrese su edad"));
     if (IngresoEdad >= 18) {
         alert("Puedes comprar bebidas alcoholicas");
-    } else if (IngresoEdad < 18) {
-        alert("No puedes comprar bebidas alcoholicas");
-    }
+    } else if (IngresoEdad < 18){
+    alert("No puedes comprar bebidas alcoholicas");
+}
+    
 
     let BebidasGin = "Ingrese marca del Gin que desea";
     let BebidasVodka = "Ingrese marca del Vodka que desea";
@@ -17,10 +18,23 @@
     let BebidasWhisky = "Ingrese marca del Whisky que desea";
     
     let IngresoBebidas = prompt("Ingrese bebida que desee:\ngin\nvodka\ncerveza\nvino\nron\nwhisky");
-    let TiposDeGin = "1- Heredero \n2-Brighton \n3-Tanqueray";
+    let ValoresGinHeredero = "Heredero: $10.285";
+    let ValoresGinBrighton = "Brighton: $5.885";
+    let ValoresGinTanqueray = "Tanqueray: $27.885";
+    let TiposDeGin1 = prompt(ValoresGinHeredero);
+    let TiposDeGin2 = prompt(ValoresGinBrighton);
+    let TiposDeGin3 = prompt(ValoresGinTanqueray);
+
+    
 
     if (IngresoBebidas == "gin") {
-        prompt(TiposDeGin);
+        if (TiposDeGin1 == "1") {
+            alert(ValoresGinHeredero);
+        } else if (TiposDeGin2 == "2") {
+            alert(ValoresGinBrighton);
+        } else if (TiposDeGin3 == "3") {
+            alert(ValoresGinTanqueray);
+        }
 
     } else if (IngresoBebidas == "vodka") {
         BebidasVodka = prompt(BebidasVodka);
@@ -40,3 +54,8 @@ console.log(IngresoDatosClientes);
 console.log(IngresoEdad);
 console.log(IngresoBebidas);
 console.log(BebidasGin);
+//console.log(BebidasVodka);
+//console.log(BebidasCerveza);
+//console.log(BebidasVino);
+//console.log(BebidasRon);
+//console.log(BebidasWhisky);
